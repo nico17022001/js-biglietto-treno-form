a84838e9b2af3dbcfdfc87a9b392143b0a358fd5
nico17022001 <nico17022001boolean@gmail.com>
2023-03-23 14:45:11 +0100

    aggiunto-contenuto

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const kmToDo = prompt ("Quanti km devi percorrere ?");
+// t0 => tempo in cui carica il browser
 
-const userName = prompt ("Come ti chiami");
+// data
+const inputName = document.querySelector('#userName');
+const InputKm = document.querySelector('#km');
+const inputAge = document.querySelector('#age');
+const buttonGenera = document.querySelector('#genera');
+const buttonAnnulla = document.querySelector('#annulla')
 
-const userAge = prompt ("Quanti anni hai ?");
+// logic
+// t1 => parte quando l'utente clicca sul pulsante GENERA
+buttonGenera.addEventListener('click', function() {
+  
+  // data
+  const name = inputName.value;
+  const km = InputKm.value;
+  const age = inputAge.value;
+  let price = 0.21 * km;
 
-let tiketPrice = (kmToDo * 0.21);
 
-if (userAge = .minorenne){
-  tiketPrice = (tiketPrice - tiketPrice * 0.2);
-}
+  // logic
+  if (age == 'minorenne') {
+    price = price * 0.8;
+  }
 
-if (userAge = .anziano){
-  tiketPrice = (tiketPrice - tiketPrice * 0.4);
-}
+  if (age == 'anziano') {
+    price = price * 0.6;
+  }
 
-console.log(kmToDo, userAge, userName,tiketPrice);
+  buttonAnnulla.addEventListener('click', function(){
+    
+  })
+
+
+  // output
+  document.querySelector('#name').innerText = name
+  document.querySelector('#userAge').innerText = 'Biglietto' + ' ' + age
+  document.querySelector('#tiketPrice').innerText = price + ' ' + 'euro'
+
+});
